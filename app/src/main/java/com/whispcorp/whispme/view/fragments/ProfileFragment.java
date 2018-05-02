406ed776089293b3a26e533e6dcaa82adfab3aed
Hugo Rosado <hugorosol17@gmail.com>
2018-05-02 18:16:47 -0500

    Profile added.

--- app/src/main/java/com/whispcorp/whispme/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/whispcorp/whispme/view/fragments/ProfileFragment.java
@@ -3,19 +3,22 @@ package com.whispcorp.whispme.view.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.whispcorp.whispme.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class WorldFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
 
-    public WorldFragment() {
+    public ProfileFragment() {
         // Required empty public constructor
     }
 
@@ -23,8 +26,12 @@ public class WorldFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_world, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        RelativeLayout profileRelativeLayout = view.findViewById(R.id.profileRelativeLayout);
+
+        return view;
     }
 
 }
