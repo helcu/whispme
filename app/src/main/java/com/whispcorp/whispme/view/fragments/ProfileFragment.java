612761be94468d84627207979e4b5a2e2253046f
Hugo Rosado <hugorosol17@gmail.com>
2018-05-04 10:37:01 -0500

    Partially added profile

--- app/src/main/java/com/whispcorp/whispme/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/whispcorp/whispme/view/fragments/ProfileFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.whispcorp.whispme.R;
@@ -21,6 +22,9 @@ import com.whispcorp.whispme.R;
 public class ProfileFragment extends Fragment {
 
     Context mContext;
+    int pos_x = 0;
+    int pos_y = 0;
+
     public ProfileFragment() {
 
     }
@@ -31,12 +35,35 @@ public class ProfileFragment extends Fragment {
         mContext = context;
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-
         RelativeLayout profileRelativeLayout = view.findViewById(R.id.profileRelativeLayout);
+        ImageView photoProfileImageView = view.findViewById(R.id.photoProfileImageView);
+
+        profileRelativeLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                    /*case MotionEvent.ACTION_DOWN:
+                        pos_x = x - layoutParams.leftMargin;
+                        pos_y = y - layoutParams.topMargin;
+                        break;
+
+*/                    case MotionEvent.ACTION_MOVE:
+                        int width = v.getLayoutParams().width;
+                        int height = v.getLayoutParams().height;
+                        v.getLayoutParams().height = v.getLayoutParams().height - 1;
+                        v.setLayoutParams(new RelativeLayout.LayoutParams(width, height));
+                        break;
+                }
+                return true;
+            }
+        });
+
 
         return view;
     }
