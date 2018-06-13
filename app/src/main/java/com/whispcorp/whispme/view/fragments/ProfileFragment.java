9e1ddb24e254d4bce61e2736f11d4407dd7c69a9
Hugo Rosado <hugorosol17@gmail.com>
2018-06-12 20:56:47 -0500

    Profile Activity updated.

--- app/src/main/java/com/whispcorp/whispme/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/whispcorp/whispme/view/fragments/ProfileFragment.java
@@ -3,6 +3,7 @@ package com.whispcorp.whispme.view.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.whispcorp.whispme.R;
+import com.whispcorp.whispme.view.adapters.ViewPagerAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,8 +24,8 @@ import com.whispcorp.whispme.R;
 public class ProfileFragment extends Fragment {
 
     Context mContext;
-    int pos_x = 0;
-    int pos_y = 0;
+    CollapsingToolbarLayout profileCollapsingToolbarLayout;
+    ViewPagerAdapter viewPagerAdapter;
 
     public ProfileFragment() {
 
@@ -34,36 +36,23 @@ public class ProfileFragment extends Fragment {
         super.onAttach(context);
         mContext = context;
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        RelativeLayout profileRelativeLayout = view.findViewById(R.id.profileRelativeLayout);
-        ImageView photoProfileImageView = view.findViewById(R.id.photoProfileImageView);
 
-        /*profileRelativeLayout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                final int x = (int) event.getRawX();
-                final int y = (int) event.getRawY();
+        Toolbar toolbar = view.findViewById(R.id.profileToolbar);
+        profileCollapsingToolbarLayout = view.findViewById(R.id.profileCollapsingToolbarLayout);
+        viewPagerAdapter = new ViewPagerAdapter(getFragmentManager());
 
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                    *//*case MotionEvent.ACTION_DOWN:
-                        pos_x = x - layoutParams.leftMargin;
-                        pos_y = y - layoutParams.topMargin;
-                        break;
+        /*viewPagerAdapter.addFragment(new AllCoursesFragment(), "All Courses");
+        viewPagerAdapter.addFragment(new CoursesCompletedFragment(), "Courses Completed");*/
 
-*//*                    case MotionEvent.ACTION_MOVE:
-                        int width = v.getLayoutParams().width;
-                        int height = v.getLayoutParams().height;
-                        v.getLayoutParams().height = v.getLayoutParams().height - 1;
-                        v.setLayoutParams(new RelativeLayout.LayoutParams(width, height));
-                        break;
-                }
-                return true;
-            }
-        });*/
-
+        profileCollapsingToolbarLayout.setTitle("My title");
+        profileCollapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.space_transparent));
+        profileCollapsingToolbarLayout.setContentScrimColor(getResources().getColor(R.color.colorPrimary));
+        profileCollapsingToolbarLayout.setStatusBarScrimColor(getResources().getColor(R.color.colorPrimaryDark));
 
         return view;
     }
